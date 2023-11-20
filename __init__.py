c4f746d8bb0bc02014d2fa5cf6781c655ecb704e
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-20 19:17:55 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from python_video.video import import frames_to_video, video_frames, video_info, video_writer_like
+from python_video.video import frames_to_video, video_frames, video_info, video_writer_like
